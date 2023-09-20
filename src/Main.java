d5da05f57d96c3907f574f9f6ba60ed9ce4864d6
aldinalj <aldinalj@gmail.com>
2023-09-20 10:17:59 +0200

    Uppgift 2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         String player = sc.nextLine();
         System.out.println("Player #1: " + player);
 
+        System.out.println("How many times do you want the loop to run?");
+        int loopCount = sc.nextInt();
+
+        for (int i = 0; i < loopCount; i++) {
+            System.out.println(i);
+        }
+
     }
 }
